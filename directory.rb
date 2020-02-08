74d78927d5dcaf562f9aafebfb5e7301675c5bb0
naeglinghaff <kate.o.haffenden@gmail.com>
2020-02-08 16:27:55 +0000

    Added methods for printing list & count of students

--- directory.rb
+++ directory.rb
@@ -14,9 +14,24 @@ students = [
 ]
 
 #prints the list of students
-puts "The students of Villains Academy"
-puts "-----------------------------"
-puts students
+def print_header
+  puts "The students of Villains Academy"
+  puts "-----------------------------"
+end
+
+def print_students(names)
+ names.each do |name|
+     puts name
+  end
+end
 
 #prints the number of students on one line
-puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+ puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print_students(students)
+print_footer(students)
+
+
