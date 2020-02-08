42479037f550203523b0f9544e089b638adbb976
naeglinghaff <kate.o.haffenden@gmail.com>
2020-02-08 16:11:28 +0000

    Added comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#prints the list of students
 puts "The students of Villains Academy"
 puts "------------------------"
 puts "Dr. Hannibal Lecter"
@@ -12,6 +13,7 @@ puts "The Joker"
 puts "Joffrey  Baratheon"
 puts "Norman Bates"
 
+#prints the number of students on one line
 print "Overall, we have "
 print 11
 puts " great students"
