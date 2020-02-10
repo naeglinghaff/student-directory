acf142e699e0a392a39d462ec1c242f8a0cc423e
naeglinghaff <kate.o.haffenden@gmail.com>
2020-02-10 22:34:53 +0000

    Global variable instated for students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+@students = []
+
 #prints above the list of students
 def print_header
   puts "The students of Villains Academy"
@@ -5,34 +7,39 @@ def print_header
 end
 
 #prints students
-def print_students(students)
- students.each_with_index do |student, index|
+def print_students()
+ @students.each_with_index do |student, index|
      puts "index: #{index + 1} #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 #prints the number of students on one line
-def print_footer(students)
- puts "Overall, we have #{students.count} great students"
+def print_footer
+ puts "Overall, we have #{@students.count} great students"
+end
+
+def prints_menu
+ puts "1. Input the students"
+  puts "2. Print out the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students
+  print_footer
 end
 
 #creates a menu for the user to choose from
 def interactive_menu
- students = []
- 
  loop do
-  puts "1. Input the students"
-  puts "2. Print out the students"
-  puts "9. Exit"
+  prints_menu
   selection = gets.chomp
-  
   case selection
     when "1"
-     students = input_students
+     @students = input_students
     when "2"
-      print_header
-      print_students(students)
-      print_footer(students)
+      show_students
       when "9"
        exit
       else
@@ -46,17 +53,15 @@ end
 def input_students
  puts "Please enter the names of the students"
  puts "To exit, press return twice"
- #empty array
- students = []
  name = gets.chomp
  #condition to loop through input until user inputs empty name
  while !name.empty? do
-   students << {name: name, cohort: :november}
-   puts "now we have #{students.count} great students"
+   @students << {name: name, cohort: :november}
+   puts "now we have #{@students.count} great students"
    name = gets.chomp
  end
  #returns the array
- students
+ @students
 end
 
 interactive_menu
