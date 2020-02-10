d3eaa0caf3095892f222535e38cec4051038281a
naeglinghaff <kate.o.haffenden@gmail.com>
2020-02-10 13:54:38 +0000

    Cohort now added with user input

--- directory-exercises.rb
+++ directory-exercises.rb
@@ -8,7 +8,7 @@ def print_students(students)
  count = 0
  while count < students.length do
   student = students[count]
-  string = "#{student[:name]} #{student[:age]}"
+  string = "#{student[:name]} #{student[:age]} #{student[:cohort]}"
   puts string.center(30)
   count += 1
  end
@@ -22,17 +22,21 @@ end
 #method for inputting students
 def input_students
  puts "Please enter the name of the student, followed by their age"
- puts "To exit, press return twice"
+ puts "To exit, press return three times"
  #empty array
  students = []
  name = gets.chomp
  age = gets.chomp
+ puts "What cohort are they joining?"
+ cohort = gets.chomp
  #condition to loop through input until user inputs empty name
- while !name.empty? && !age.empty? do
-   students << {name: name, cohort: :november, age: age}
+ while !name.empty? && !age.empty? && !cohort.empty? do
+   students << {name: name, cohort: cohort.to_sym, age: age}
    puts "now we have #{students.count} great students"
    name = gets.chomp
    age = gets.chomp
+   puts "What cohort are they joining?"
+   cohort = gets.chomp
  end
  #returns the array
  students
