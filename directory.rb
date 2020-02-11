de237f4c27cd182d466dc1c1d36e8ff9a41e9892
naeglinghaff <kate.o.haffenden@gmail.com>
2020-02-11 09:09:43 +0000

    Save student data as a file method added

--- directory.rb
+++ directory.rb
@@ -9,7 +9,7 @@ end
 #prints students
 def print_student_list
  @students.each_with_index do |student, index|
-     puts "index: #{index + 1} #{student[:name]} (#{student[:cohort]} cohort)"
+   puts "index: #{index + 1} #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -21,15 +21,16 @@ end
 #outputs the menu options
 def prints_menu
  puts "1. Input the students"
-  puts "2. Print out the students"
-  puts "9. Exit"
+ puts "2. Print out the students"
+ puts "3. Save the student data to a file"
+ puts "9. Exit"
 end
 
 #prints the students for option 2
 def show_students
-  print_header
-  print_student_list
-  print_footer
+ print_header
+ print_student_list
+ print_footer
 end
 
 #processes the users selection from the menu
@@ -39,10 +40,12 @@ def processing(selection)
     @students = input_students
    when "2"
     show_students
+   when "3"
+    save_students
    when "9"
     exit
    else
-     puts "I don't know what you meant, please try again"
+    puts "I don't know what you meant, please try again"
  end
 end
 
@@ -54,6 +57,16 @@ def interactive_menu
  end
 end
 
+def save_students
+ file = File.open("students.csv", "w")
+ @students.each do |student|
+  student_data = [student[:name], student[:cohort]]
+  csv_line = student_data.join(",")
+  file.puts csv_line
+ end
+ file.close
+end
+
 #method for inputting students
 def input_students
  puts "Please enter the names of the students"
@@ -61,9 +74,9 @@ def input_students
  name = gets.chomp
  #condition to loop through input until user inputs empty name
  while !name.empty? do
-   @students << {name: name, cohort: :november}
-   puts "now we have #{@students.count} great students"
-   name = gets.chomp
+  @students << {name: name, cohort: :november}
+  puts "now we have #{@students.count} great students"
+  name = gets.chomp
  end
  #returns the array
  @students
