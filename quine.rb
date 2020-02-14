f589aa463a2113642441ff57c69561da6071b7d2
naeglinghaff <kate.o.haffenden@gmail.com>
2020-02-14 12:31:59 +0000

    Practice quine program outputting sourcecode

---
+++ quine.rb
@@ -0,0 +1,4 @@
+#initialise a string with a placeholder using %
+#puts this string to the console interpoling the string into itself via the placeholder
+string = "string = %pstring; puts string%%string" 
+puts string%string
